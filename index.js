53f624768c8ae56a5fabc7ca035aa57ea1a91618
AlexndreSilva <98479499+AlexndreSilva@users.noreply.github.com>
2022-08-31 17:55:17 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-//chamando o modulo
-const meuModulo = require('./meus_modulos/modulos')
-
-// criando const com o modulo pra simplificar a sintaxe
-const soma = meuModulo.soma
-
-// chamando o modulo inportado
-
-soma(9,20)
